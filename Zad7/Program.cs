283c1bf80d98937d65718da959bd9057f1cb0beb
Alen Štruklec <m3talen@gmail.com>
2016-10-28 19:56:23 +0200

    Added Zad7

---
+++ Zad7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Zad7
+{
+    internal class Program
+    {
+        //private static long _result;
+
+        private static void Main(string[] args)
+        {
+            var task = Task.Run(() => MainAsync());
+            Task.WaitAll(task);
+
+            Console.Read();
+        }
+
+        private static async void MainAsync()
+        {
+            Console.WriteLine(await FactorialDigitSum(1));
+            Console.WriteLine(await FactorialDigitSum(1));
+            Console.WriteLine(await FactorialDigitSum(2));
+            Console.WriteLine(await FactorialDigitSum(3));
+            Console.WriteLine(await FactorialDigitSum(4));
+            Console.WriteLine(await FactorialDigitSum(5));
+            Console.WriteLine(await FactorialDigitSum(6));
+            Console.WriteLine(await FactorialDigitSum(7));
+        }
+
+        public static async Task<long> FactorialDigitSum(int num)
+        {
+            var task = Task.Run(() => GetFactorialDigitSum(num));
+            await task;
+            return task.Result;
+        }
+
+        private static long GetFactorialDigitSum(int num)
+        {
+            long result = num;
+            for (var i = 1; i < num; i++)
+                result = result * i;
+            return result;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+#Pitanje 1:
+~5 sekundi.
+
+#Pitanje 2:
+Na jednoj dretvi.
+
+#Pitanje 3:
+1,1588161 sekundi
+
+#Pitanje 4:
+Na 5 dretvi.
+
+#Pitanje 5:
+Parallel  calls  finished  0,2693 ms.
+Sync  operation  calls  finished  0,0004 ms.
+(Optimize code enabled)
+
+
