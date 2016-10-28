da6a08bd470273f7ee4b8a1e4a7fce842e0de59b
Alen Štruklec <m3talen@gmail.com>
2016-10-28 20:51:38 +0200

    Added Zad8, and fixed Zad7

--- Zad7/Program.cs
+++ Zad7/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Zad7
 {
-    internal class Program
+    public class Program
     {
-        //private static long _result;
-
         private static void Main(string[] args)
         {
             var task = Task.Run(() => MainAsync());
@@ -19,23 +18,20 @@ namespace Zad7
         private static async void MainAsync()
         {
             Console.WriteLine(await FactorialDigitSum(1));
-            Console.WriteLine(await FactorialDigitSum(1));
             Console.WriteLine(await FactorialDigitSum(2));
-            Console.WriteLine(await FactorialDigitSum(3));
             Console.WriteLine(await FactorialDigitSum(4));
-            Console.WriteLine(await FactorialDigitSum(5));
-            Console.WriteLine(await FactorialDigitSum(6));
-            Console.WriteLine(await FactorialDigitSum(7));
+            Console.WriteLine(await FactorialDigitSum(8));
+            Console.WriteLine(await FactorialDigitSum(16));
         }
 
         public static async Task<long> FactorialDigitSum(int num)
         {
-            var task = Task.Run(() => GetFactorialDigitSum(num));
+            Task<int> task = Task.Run(() => (GetFactorial(num).ToString().Sum(c=>c-'0')));
             await task;
             return task.Result;
         }
 
-        private static long GetFactorialDigitSum(int num)
+         static long GetFactorial(int num)
         {
             long result = num;
             for (var i = 1; i < num; i++)
---
+++ Zad8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zad8
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+             var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+             Console.Read();
+         }
+
+         private static void LetsSayUserClickedAButtonOnGuiMethod()
+         { 
+             var result = GetTheMagicNumber();
+             Console.WriteLine(result);
+         }
+
+         private static long GetTheMagicNumber()
+         {
+             return IKnowIGuyWhoKnowsAGuy().Result;
+         }
+
+         private static async Task<long> IKnowIGuyWhoKnowsAGuy()
+         {
+             var t1 = IKnowWhoKnowsThis(10);
+             var t2 = IKnowWhoKnowsThis(5);
+             await Task.WhenAll(t1, t2);
+             return t1.Result + t2.Result;
+         } 
+
+         private static async Task<long> IKnowWhoKnowsThis(int n)
+         {
+             return await Zad7.Program.FactorialDigitSum(n);
+         }
+        }
+}
