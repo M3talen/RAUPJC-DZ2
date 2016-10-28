d3adb03c0088bd01216934ef988ebb83bcf974a1
Alen Štruklec <m3talen@gmail.com>
2016-10-28 19:27:28 +0200

    Added Zad6

--- Zad3/Program.cs
+++ Zad3/Program.cs
@@ -9,11 +9,11 @@ namespace Zad3
         {
             var integers = new[] {1, 2, 2, 2, 3, 3, 4, 5};
             var data = integers.GroupBy(x => x).Distinct().ToArray();
-            var strings = new string[integers.Length];
+            var strings = new string[data.Length];
             foreach (var n in data)
             {
-                strings[n.Key] = $"Broj {n.Key} ponavlja se {n.Count()} puta";
-                Console.Out.WriteLine(strings[n.Key]);
+                strings[n.Key-1] = $"Broj {n.Key} ponavlja se {n.Count()} puta";
+                Console.Out.WriteLine(strings[n.Key-1]);
             }
             Console.In.Read();
         }
---
+++ Zad6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zad6
+{
+    internal class Program
+    {
+        private static readonly object Lock = new object();
+        private static void Main(string[] args)
+        {
+            var results = new List<int>();
+            Parallel.For(0, 100, i =>
+            {
+                Thread.Sleep(1);
+                lock (Lock)
+                {
+                    results.Add(i*i);
+                }
+            });
+            Console.WriteLine("Bag length should be 100. Length is {0}", results.Count);
+
+            var iterations = new ConcurrentBag<int>();
+            Parallel.For(0, 100, i =>
+            {
+                Thread.Sleep(1);
+                iterations.Add(i);
+            });
+            Console.WriteLine("ConcurrentBag length should be 100. Length is {0}", iterations.Count);
+
+            Console.Read();
+        }
+
+        public static void LongOperation(string taskName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("{0}  Finished. Executing  Thread: {1}", taskName, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
